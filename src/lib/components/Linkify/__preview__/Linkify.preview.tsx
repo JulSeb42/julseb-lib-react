e0f7832db304c6f830444779896fa48591d3648d
JulSeb42 <julien.sebag@me.com>
2024-07-10 18:49:14 +0200

    Add highlighted text

--- src/lib/components/Linkify/__preview__/Linkify.preview.tsx
+++ src/lib/components/Linkify/__preview__/Linkify.preview.tsx
@@ -1,7 +1,6 @@
 /*=============================================== LinkifyPreview ===============================================*/
 
 import { Linkify } from "../../../"
-import {  } from "../../../types"
 import type { LinkifyProps } from "../../../types/components-props"
 import type { ComponentPreview } from "../../../../data/components"
 
@@ -10,6 +9,10 @@ export const linkifyPreview: ComponentPreview<LinkifyProps, typeof Linkify> = {
     component: Linkify,
     category: "components",
     props: [
-        { previewTitle: "Default" },
+        {
+            previewTitle: "Default",
+            children: "Hello world https://julien-sebag.com/ foo baz",
+            blank: true,
+        },
     ],
 }
