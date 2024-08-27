652c0c4d611371cda0b945240aafb13913a01a05
Julien Sebag <juliensebag@mbp-de-julien.home>
2024-08-27 14:31:54 +0200

    Add scripts for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "node:path"
@@ -8,17 +9,24 @@ import pluginChecker from "vite-plugin-checker"
 export default defineConfig({
     plugins: [
         react(),
-        dts({ insertTypesEntry: true }),
+        dts({
+            insertTypesEntry: true,
+            exclude: [
+                "./src/lib/components/*/__preview__",
+                "./src/lib/components/*/__tests__",
+            ],
+        }),
         pluginChecker({ typescript: true }),
     ],
 
     build: {
         lib: {
-            entry: path.resolve(__dirname, "src/lib/index.ts"),
-            name: "tsx-library-julseb",
+            entry: path.resolve(__dirname, "./src/lib/index.ts"),
+            name: "julseb-lib-react",
             formats: ["es", "umd", "cjs"],
-            fileName: format => `tsx-library-julseb.${format}.js`,
+            fileName: format => `index.${format}.js`,
         },
+
         rollupOptions: {
             external: [
                 "react",
@@ -37,6 +45,13 @@ export default defineConfig({
                     react: "React",
                     "react-dom": "ReactDOM",
                     "styled-components": "styled",
+                    "ts-utils-julseb": "ts-utils-julseb",
+                    "react-router-dom": "react-router-dom",
+                    "react-inlinesvg": "react-inlinesvg",
+                    "fuse.js": "fuse.js",
+                    "react-helmet-async": "react-helmet-async",
+                    "react-syntax-highlighter": "react-syntax-highlighter",
+                    "markdown-to-jsx": "markdown-to-jsx",
                 },
             },
         },
