cab702c2a899b06e877e7e37724e9d46994266db
JulSeb42 <julien.sebag@me.com>
2025-03-14 13:27:17 +0100

    Clean + replace wrapper as div with section

--- src/lib/components/Wrapper/styles.tsx
+++ src/lib/components/Wrapper/styles.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components"
 import { MEDIA, Mixins, setDefaultTheme } from "../../"
 import type { LibAllColors, LibSpacers } from "../../types"
 
-const StyledWrapper = styled.div<{
+const StyledWrapper = styled.section<{
     $gap: LibSpacers
     $backgroundColor: LibAllColors
 }>`
